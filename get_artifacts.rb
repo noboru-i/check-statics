db193474591cfef625cb9f4640c40ed2c0656563
noboru-i <ishikura.noboru@gmail.com>
2015-11-18 17:52:51 +0900

    get artifacts

---
+++ get_artifacts.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/env ruby
+
+host = 'https://circleci.com/api/v1/'
+user = 'noboru-i'
+repo = 'kyouen-android'
+token = 'XXX'
+
+require 'open-uri'
+require 'uri'
+require 'json'
+
+puts 'get latest repo'
+res = open("#{host}project/#{user}/#{repo}?circle-token=#{token}")
+p res.status[0]
+exit if res.status[0] != '200'
+
+result = JSON.parse(res.read)
+
+latest_build = result.find do |elm|
+  elm['branch'] == 'develop' && elm['status'] == 'success'
+end
+
+build_num = latest_build['build_num']
+
+puts 'get artifact list'
+res = open("#{host}project/#{user}/#{repo}/#{build_num}/artifacts?circle-token=#{token}")
+p res.status[0]
+exit if res.status[0] != '200'
+
+result = JSON.parse(res.read)
+
+artifact_names = [
+  'brakeman.json',
+  'eslint.xml',
+  'rails_best_practices_output.xml',
+  'reek.xml',
+  'rubocop.xml',
+  'scsslint.xml'
+]
+result.each do |elm|
+  found = artifact_names.find do |name|
+    elm['path'].include? name
+  end
+  next if found.nil?
+
+  url = elm['url']
+  puts "download from #{url}"
+  file_name = File.basename(url)
+  open(file_name, 'wb') do |output|
+    open("#{url}?circle-token=#{token}") do |data|
+      output.write(data.read)
+    end
+  end
+end
