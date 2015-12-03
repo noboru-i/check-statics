626c9d80b5d80eaba16b133bfc6260edcc6055f3
noboru-i <ishikura.noboru@gmail.com>
2015-12-03 14:31:34 +0900

    use setting.yml

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # check-statics
+
+## How to use
+
+First, you set target repo/branch and CircleCI token to `setting.yml`.
+
+Second, you execute `ruby get_artifacts.rb`. You can get some artifacts.
+
+Third, you execute `ruby statics.rb`. You can see grouped lint warning/error counts.
--- get_artifacts.rb
+++ get_artifacts.rb
@@ -1,29 +1,29 @@
 #!/usr/bin/env ruby
 
 host = 'https://circleci.com/api/v1/'
-user = 'noboru-i'
-repo = 'kyouen-android'
-token = 'XXX'
+
+require 'yaml'
+config = YAML.load_file('setting.yml')
 
 require 'open-uri'
 require 'uri'
 require 'json'
 
 puts 'get latest repo'
-res = open("#{host}project/#{user}/#{repo}?circle-token=#{token}")
+res = open("#{host}project/#{config['user']}/#{config['repo']}?circle-token=#{config['token']}")
 p res.status[0]
 exit if res.status[0] != '200'
 
 result = JSON.parse(res.read)
 
 latest_build = result.find do |elm|
-  elm['branch'] == 'develop' && elm['status'] == 'success'
+  elm['branch'] == config['branch'] && elm['status'] == 'success'
 end
 
 build_num = latest_build['build_num']
 
 puts 'get artifact list'
-res = open("#{host}project/#{user}/#{repo}/#{build_num}/artifacts?circle-token=#{token}")
+res = open("#{host}project/#{config['user']}/#{config['repo']}/#{build_num}/artifacts?circle-token=#{config['token']}")
 p res.status[0]
 exit if res.status[0] != '200'
 
@@ -47,7 +47,7 @@ result.each do |elm|
   puts "download from #{url}"
   file_name = File.basename(url)
   open(file_name, 'wb') do |output|
-    open("#{url}?circle-token=#{token}") do |data|
+    open("#{url}?circle-token=#{config['token']}") do |data|
       output.write(data.read)
     end
   end
